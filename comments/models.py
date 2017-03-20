e369deb0752eb2ccd2fe965a6b92b4294dbbe96e
SudoBobo <ivan.koptelov@phystech.edu>
2017-03-20 18:49:21 +0300

    ^^

--- comments/models.py
+++ comments/models.py
@@ -5,6 +5,8 @@ from django.db import models
 class Comment(models.Model):
 
     post = models.ForeignKey('posts.Post')
+    author = models.ForeignKey(settings.AUTH_USER_MODEL)
+
     text = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
